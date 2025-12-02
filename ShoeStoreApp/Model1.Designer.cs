5f154315f27204b5676ba7201ba662df0d3fef73
maksim <m>
2025-12-02 16:49:38 +0300

    Добавлены ресурсы

--- ShoeStoreApp/Model1.Designer.cs
+++ ShoeStoreApp/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\mm\source\repos\ShoeStoreApp\ShoeStoreApp\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\maksi\Source\Repos\UP0101_Zheltobryukh\ShoeStoreApp\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
